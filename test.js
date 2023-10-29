7397aaa72e5955fc307e94233420d784b4f32d1e
Nobita <91586202+2567345038@users.noreply.github.com>
2023-10-29 22:30:58 +0800

    更新 test.js

--- test.js
+++ test.js
@@ -30,11 +30,12 @@
   };
 
   // 获取题目数据
+var data;
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://raw.githubusercontent.com/2567345038/metrodriver12/main/Nobita.js', true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      var data = JSON.parse(xhr.responseText);
+      data = JSON.parse(xhr.responseText);
       autoAnswer(data);
     }
   };
